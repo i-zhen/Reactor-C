74e04ee272b1790acbed419b07bc2ac31937e1fd
Z.Y <iamzhenyi@gmail.com>
2015-12-10 23:08:31 +0000

    update

--- out/production/CT-15-16/a.c
+++ out/production/CT-15-16/a.c
@@ -1,17 +1,10 @@
 #include "io.h"
 int h;
 int foo() {
-  if('s'>'c')
-	 while(1>9) return 1;
-   else {
-     h=9+7;
-     {
-       return 'd';
-     }
-   }
-	return 0;
+{
+
 }
 
-void main(){
+int main(){
   
 }
