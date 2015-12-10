515b419358f441274a553332be19c6774ba1e8a5
Z.Y <iamzhenyi@gmail.com>
2015-12-10 00:06:42 +0000

    up

--- out/production/CT-15-16/a.c
+++ out/production/CT-15-16/a.c
@@ -1,7 +1,8 @@
 #include "io.h"
 
-void foo() { return x; }
+void foo(){
+    return 9*7;
+}
 
 void main() {
-    int x;
 }
--- src/sem/TypeCheckVisitor.java
+++ src/sem/TypeCheckVisitor.java
@@ -6,81 +6,139 @@ public class TypeCheckVisitor extends BaseSemanticVisitor<Type> {
 
 	@Override
 	public Type visitBlock(Block b) {
-		// To be completed...
+		for(VarDecl vd : b.params)
+            vd.accept(this);
+        for(Stmt st : b.stmts)
+            st.accept(this);
 		return null;
 	}
 
 	@Override
 	public Type visitProcedure(Procedure p) {
-		// To be completed...
-		return null;
+        for(VarDecl vd : p.params)
+            vd.accept(this);
+        p.block.accept(this);
+		return p.type;
 	}
 
 	@Override
 	public Type visitProgram(Program p) {
-		// To be completed...
+		for(VarDecl vd : p.varDecls)
+            vd.accept(this);
+        for(Procedure ps : p.procs)
+            ps.accept(this);
+        p.main.accept(this);
 		return null;
 	}
 
 	@Override
 	public Type visitVarDecl(VarDecl vd) {
-		// To be completed...
+		if (vd.type == Type.VOID)
+			error("Variable cannot be VOID type");
 		return null;
 	}
 
 	@Override
 	public Type visitVar(Var v) {
-		// To be completed...
-		return null;
+		v.type = v.vd.type;
+		return v.type;
 	}
 
 	@Override
-	public Type visitIntLiteral(IntLiteral i){
-		return null;
-	}
+	public Type visitIntLiteral(IntLiteral i){ return Type.INT; }
 
 	@Override
 	public Type visitStrLiteral(StrLiteral s){
-		return null;
+		return Type.STRING;
 	}
 
 	@Override
 	public Type visitChrLiteral(ChrLiteral c){
-		return null;
+		return Type.CHAR;
 	}
 
 	@Override
 	public Type visitBinOp(BinOp b){
-		return null;
+        Type lhsT = b.lhs.accept(this);
+        Type rhsT = b.rhs.accept(this);
+        switch (b.op){
+            case ADD: case SUB: case DIV: case MOD: case MUL:
+                if(lhsT == Type.INT && rhsT == Type.INT){
+                    b.type = Type.INT;
+                    return Type.INT;
+                } else
+                    error("Type mismatch : BinOp :" + lhsT.toString() + " " + rhsT.toString());
+                break;
+            default:
+                if(lhsT == Type.VOID || rhsT == Type.VOID)
+                    error("IMPOSSIBLE type : VOID");
+                else if(lhsT == rhsT){
+                    b.type = lhsT;
+                    return b.type;
+                } else
+                    error("Type mismatch : " + lhsT.toString() + " " + rhsT.toString());
+        }
+        return null;
 	}
 
 	@Override
 	public Type visitFunCallExpr(FunCallExpr f){
+        if(f.p.params.size() != f.args.size())
+            error("Number of args does not match : FunCallExpr");
+        Boolean result = true;
+        for(int pos = 0; pos < f.args.size(); pos++)
+            result &= f.args.get(pos).accept(this) == f.p.params.get(pos).accept(this);
+        if(!result) {
+            error("Type of args does not match : FunCallExpr");
+            f.type = f.p.type;
+            return f.type;
+        }
 		return null;
 	}
 
 	@Override
 	public Type visitReturn(Return r){
+        if (r.exp != null)
+            return r.exp.accept(this);
 		return null;
 	}
 
 	@Override
 	public Type visitFunCallStmt(FunCallStmt f){
-		return null;
+        if(f.p.params.size() != f.args.size())
+            error("Number of args does not match : FunCallStmt");
+        Boolean result = true;
+        for(int pos = 0; pos < f.args.size(); pos++)
+            result &= f.args.get(pos).accept(this) == f.p.params.get(pos).accept(this);
+        if(!result)
+            error("Type of args does not match : FunCallStmt");
+        return null;
 	}
 
 	@Override
 	public Type visitWhile(While w){
+        w.exp.accept(this);
+        w.stmt.accept(this);
 		return null;
 	}
 
 	@Override
 	public Type visitIf(If i){
+        i.exp.accept(this);
+        i.ifstmt.accept(this);
+        if(i.elsestmt != null)
+            i.elsestmt.accept(this);
 		return null;
 	}
 
 	@Override
 	public Type visitAssign(Assign a){
+        Type expT = a.exp.accept(this);
+        a.var.accept(this);
+        if(a.var.type == expT)
+            return expT;
+        else
+            error("Type mismatch : Assign :" + a.var.type.toString() + " " + expT.toString());
 		return null;
 	}
 }
