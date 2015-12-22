54987e72f636490f95a52d6c70cfb0dc5bbe1fc6
Z.Y <iamzhenyi@gmail.com>
2015-12-22 18:00:17 +0000

    update

--- src/gen/CodeGenerator.java
+++ src/gen/CodeGenerator.java
@@ -1,10 +1,13 @@
 package gen;
 
 import ast.Program;
+import jdk.internal.org.objectweb.asm.*;
 
 public class CodeGenerator {
     public void emitProgram(Program program) {
         // TODO: emit a java class named Main that contains your program and write it to the file out/Main.class
         // to do this you will need to write a visitor which traverses the AST and emit the different global variables as static fields and the different procedures as static methods.
     }
+
 }
+
