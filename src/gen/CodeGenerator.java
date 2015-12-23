f5e175420b8ec98cca3eeb8a3fa867c49b030236
Z.Y <iamzhenyi@gmail.com>
2015-12-23 02:34:31 +0000

    update

--- src/gen/CodeGenerator.java
+++ src/gen/CodeGenerator.java
@@ -17,7 +17,6 @@ public class CodeGenerator {
         mv.visitMaxs(1, 1);
         mv.visitEnd();
         cw.visitEnd();
-
         // TODO: emit a java class named Main that contains your program and write it to the file out/Main.class
         // to do this you will need to write a visitor which traverses the AST and emit the different global variables as static fields and the different procedures as static methods.
         writeClass(cw);
